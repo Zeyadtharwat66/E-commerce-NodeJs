25e4dabcb451aaab972dc7c0b0812093f67b1f55
Zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2026-09-19 21:09:02 +0300

    Improve API error handling

--- middleware/errormiddleware.js
+++ middleware/errormiddleware.js
@@ -14,12 +14,30 @@ const sendErrorForProd = (err, res) =>
 
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorForDev(err, res);
-  } else {
-    sendErrorForProd(err, res);
+  err.status = err.status || "error";
+
+  if (err.name === "CastError") {
+    err.statusCode = 400;
+    err.status = "fail";
+    err.message = "Invalid resource id";
   }
+
+  if (err.name === "ValidationError") {
+    err.statusCode = 400;
+    err.status = "fail";
+  }
+
+  if (err.code === 11000) {
+    err.statusCode = 409;
+    err.status = "fail";
+    err.message = "A category with this name already exists";
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return sendErrorForDev(err, res);
+  }
+
+  return sendErrorForProd(err, res);
 };
 
 module.exports = globalError;
